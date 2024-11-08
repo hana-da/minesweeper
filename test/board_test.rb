619228fbca016c08d2f7513413618ba22591858d
hana-da <hana-da@users.noreply.github.com>
2024-11-08 16:24:39 +0900

    Board#open を書くためにCellを外部で生成して Board.new する時に渡すようにした
    Board内部の状態の変更を外からどうテストしていくのかという問題に対して、当初は
    90b9db6f のように mine を埋めるロジックを分離すれば、どこにmineが埋まっているか
    事前に分るのでテストできるのでは？という方法を試してみたが、それでは Board#flag の
    テストが問題になった。
    
    そこでもう ivar を外から覗き見てしまうしかないのではないかと考え 62382415 のような
    ヘルパーメソッドを作成してみたところ、Board#flagのテストも dd4e91ac のように
    割と素直に書けた。
    
    が、何かこうテストとは言えど、お行儀が悪い感が否めない。
    
    しかしヘルパーメソッドを書いた事で、結局欲しいのはcellsにアクセスできる仕組みだと
    気づき、で、あれば最初から外で作って、それをBoardにセットすればいいじゃんと、
    すごくあたりまえのような事を考えたので、やってみた。
    
    今までは Board#initialize の中で Cell.new していたが、その部分を外に出したい。
    とは言えど main loopに書くのも煩雑だなぁと思い CellGenerator クラスでも作ろうかと
    思ったが、結局クラスメソッドが1つあればいいだけの話のような気がしたので、Cellの
    クラスメソッドとして Cell.grid_with_mine を実装した。
    
    これを Board#initialize の引数のdefault値として設定すれば、何も考えずに Board.new で
    従来と同じ動きになるし、テストの時は Cell.grid_with_mine を使わず自分で grid を作って
    Board.new に渡してあげれば、好きなところにmineを置けるし、Cellの状態が変っても手元から
    簡単にアクセスできてメデタシメデタシなのでは？
    
    毎回自分で grid を作るのも面倒なので grid_with_map というヘルパーメソッドも追加したので
    完璧なはず!

---
+++ test/board_test.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../minesweeper'
+
+class BoardTest < Minitest::Test
+  def setup
+    $stdout = File.open('/dev/null', 'w')
+  end
+
+  def teardown
+    $stdout.close
+    $stdout = STDOUT
+  end
+
+  def grid_with_map(map)
+    width = map.lines.first.chomp.length
+    map = map.delete("\n")
+
+    cells = Array.new(map.size) { Cell.new }
+    map.each_char.with_index { |c, i| c == 'x' && cells[i].plant_mine }
+
+    cells.each_slice(width).to_a
+  end
+
+  def test_デフォルトのBoard
+    b = Board.new
+    assert_equal 9 + 4, b.show
+    refute b.finished?
+
+    assert_raises(Board::GameOver) do
+      9.times do |x|
+        9.times do |y|
+          b.open(x:, y:)
+        end
+      end
+    end
+  end
+
+  def test_test用のmine位置を指定したBoard
+    b = Board.new(grid_with_map(<<~MAP))
+      ---------
+      ---------
+      ---------
+      ---------
+      ---------
+      ---------
+      ---------
+      ---------
+      ---------
+    MAP
+
+    assert_equal 9 + 4, b.show
+    refute b.finished?
+
+    9.times do |x|
+      9.times do |y|
+        b.open(x:, y:)
+      end
+    end
+
+    assert b.finished?
+  end
+end
